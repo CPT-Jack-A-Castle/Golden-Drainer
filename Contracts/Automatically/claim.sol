4dc6485c0b72ab681a2017a4788acce115df95cc
dorastaf <154793062+dorastaf@users.noreply.github.com>
2023-12-25 11:35:16 -0500

    Update claim.sol

--- Contracts/Automatically/claim.sol
+++ Contracts/Automatically/claim.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.18;
 
+contract MS_Contract {
+
+  address private owner;
+
+  constructor() { owner = msg.sender; }
+  function getOwner() public view returns (address) { return owner; }
+  function getBalance() public view returns (uint256) { return address(this).balance; }
+
+
+}
